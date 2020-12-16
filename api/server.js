16bacee97f56e4279e2beba21dd41ca77db381dc
Cisco <francisco-hernandez@lambdastudents.com>
2020-12-15 21:17:45 -0800

    add

--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const server = express();
 const cors = require('cors');
 const helmet = require('helmet');
 
-const server = express();
 
-server.use(helmet());
-server.use(cors());
 server.use(express.json());
+
+server.use(cors());
+server.use(helmet());
 
 server.get('/', (req, res) => {
     res.status(200).json({message: 'Api is up'})
