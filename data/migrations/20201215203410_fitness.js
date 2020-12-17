444e572721f4d2417ff5b53018d8e0a2d1a0c1b4
eric capiz <ericcapiz@gmail.com>
2020-12-16 22:06:19 -0600

    created tables

--- data/migrations/20201215203410_fitness.js
+++ data/migrations/20201215203410_fitness.js
@@ -1,29 +1,55 @@
-
-exports.up = function(knex) {
-    return knex.schema
-
-    .createTable('users', users => {
-        users.increments();
-        
-        users.string('firstName', 255);
-
-        users.string('lastname', 255);
-
-        users.string('username', 255)
+exports.up = async function(knex) {
+    await knex.schema.createTable("users", users=>{
+        users
+            .increments("id");
+        users
+            .string("name", 64)
             .notNullable()
-            .unique();
-
-        users.string('password', 255)
-            .notNullable();
-
-        users.string('email', 255)
+        users
+            .string("username",255)
             .notNullable()
-            .unique();
-        
+            .unique()
+        users
+            .string("password",255)
     })
+
+    await knex.schema.createTable("roles", roles=>{
+        roles
+            .integer("user_id")
+            .notNullable()
+            .references("users_id")
+            // .inTable("users")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        roles
+            .enu("role",["User","Instructor"])
+            .notNullable();
+        roles
+            .primary(["user_id","role"])
+    })
+
+    await knex.schema.createTable("classes", classes=>{
+       classes
+            .increments()
+        classes
+            .string("name",255)
+            .string("type",255)
+            .string("startTime",255)
+            .string("duration",255)
+            .string("intensityLevel",255)
+            .string("location",255)
+            .integer("RegisteredAttendees",)
+            .integer("classSize")
+
+    })
+
+    
    
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists("users");
+    return knex.schema
+        .dropTableIfExists("classes")
+        .dropTableIfExists("roles")
+        .dropTableIfExists("users");
 };
