8c7d12b2f6f52243adfda26ee048762eedb867d4
Cisco <francisco-hernandez@lambdastudents.com>
2021-01-05 20:00:32 -0800

    9.5

--- users/user-router.js
+++ users/user-router.js
@@ -7,25 +7,25 @@ const restricted = require("../auth/restricted-middleware.js");
 const checkRole = require("../auth/check-role-middlware");
 
 
-router.get("/", restricted, async (req, res, next) => {
-  try {
-    const users = await Users.find();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(404)
-    next({ apiCode: 501, apiMessage: 'db error getting users', ...err })
-  }
-});
+// router.get("/", restricted, async (req, res, next) => {
+//   try {
+//     const users = await Users.find();
+//     res.status(200).json(users);
+//   } catch (err) {
+//     res.status(404)
+//     next({ apiCode: 501, apiMessage: 'db error getting users', ...err })
+//   }
+// });
 
-router.get("/", (req, res) => {
-    db("users")
-      .then(users => {
-        res.json(users);
-      })
-      .catch(err => {
-        res.status(500).json({ message: "Failed to get users" });
-      });
-  });
+// router.get("/", (req, res) => {
+//     db("users")
+//       .then(users => {
+//         res.json(users);
+//       })
+//       .catch(err => {
+//         res.status(500).json({ message: "Failed to get users" });
+//       });
+//   });
 
 
 
