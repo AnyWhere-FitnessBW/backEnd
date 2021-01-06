40e56a919aa33e51181b6d031c736bad37c5d7f5
Cisco <francisco-hernandez@lambdastudents.com>
2021-01-05 20:23:01 -0800

    56

--- api/server.js
+++ api/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const UserRouter = require('../users/user-router.js');
+// const AuthRouter = require('../auth/auth-router.js')
 
 const server = express();
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
@@ -12,9 +15,9 @@ server.get('/', (req, res) => {
     res.status(200).json({message: 'Api is up'})
 });
 
-// const UserRouter = require('../users/user-router.js');
-const AuthRouter = require('../auth/auth-router.js')
 
+server.use("/api/users", UserRouter)
+// server.use("/api/auth", AuthRouter)
 
 
 
